d9e10fc1555b552d17e2f5683e22b3bd5fb1bc16
MarkoSh <markhost@yandex.ru>
2018-10-22 21:52:55 +0300

    Забыл про файловую

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var syntax        = 'sass'; // Syntax: sass or scss;
 
-var gulp          = require('gulp'),
-		gutil         = require('gulp-util' ),
-		sass          = require('gulp-sass'),
-		browserSync   = require('browser-sync'),
-		concat        = require('gulp-concat'),
-		uglify        = require('gulp-uglify'),
-		cleancss      = require('gulp-clean-css'),
-		rename        = require('gulp-rename'),
-		autoprefixer  = require('gulp-autoprefixer'),
-		notify        = require("gulp-notify"),
-		rsync         = require('gulp-rsync');
+var	fs	      = require('fs'),
+	gulp          = require('gulp'),
+	gutil         = require('gulp-util' ),
+	sass          = require('gulp-sass'),
+	browserSync   = require('browser-sync'),
+	concat        = require('gulp-concat'),
+	uglify        = require('gulp-uglify'),
+	cleancss      = require('gulp-clean-css'),
+	rename        = require('gulp-rename'),
+	autoprefixer  = require('gulp-autoprefixer'),
+	notify        = require("gulp-notify"),
+	rsync         = require('gulp-rsync');
 
 gulp.task('browser-sync', function() {
 	browserSync({
