081a06f4a31c8a8522da24d042e5cc1015621f6a
Алексей <agragregra@ya.ru>
2025-02-09 16:20:23 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 let syntax     = 'sass', // Syntax - .sass or .scss
-		fileswatch = 'html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload
-		gmWatch    = false // true/false GraphicsMagick watching "img/_src" folder.
-		                   // Install gm Linux/WSL: sudo apt update; sudo apt install graphicsmagick
-		                   // Install gm Git Bash: https://sourceforge.net/projects/graphicsmagick/files/
+    fileswatch = 'html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload
+    gmWatch    = false // true/false GraphicsMagick watching "img/_src" folder.
+                       // Install gm Linux/WSL: sudo apt update; sudo apt install graphicsmagick
+                       // Install gm Git Bash: https://sourceforge.net/projects/graphicsmagick/files/
 
 import pkg from 'gulp'
 const { src, dest, parallel, series, watch } = pkg
@@ -21,79 +21,79 @@ import imageResize   from 'gulp-image-resize'
 import {deleteAsync} from 'del'
 
 function browsersync() {
-	browserSync.init({
-		server: {
-			baseDir: 'app/'
-		},
-		ghostMode: { clicks: false },
-		notify: false,
-		online: true,
-		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
-	})
+  browserSync.init({
+    server: {
+      baseDir: 'app/'
+    },
+    ghostMode: { clicks: false },
+    notify: false,
+    online: true,
+    // tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+  })
 }
 
 function scripts() {
-	return src([
-		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js', // Always at the end
-		])
-	.pipe(concat('scripts.min.js'))
-	// .pipe(uglify()) // Mifify js (opt.)
-	.pipe(dest('app/js'))
-	.pipe(browserSync.stream())
+  return src([
+    'app/libs/jquery/dist/jquery.min.js',
+    'app/js/common.js', // Always at the end
+    ])
+  .pipe(concat('scripts.min.js'))
+  // .pipe(uglify()) // Mifify js (opt.)
+  .pipe(dest('app/js'))
+  .pipe(browserSync.stream())
 }
 
 function styles() {
-	return src([`app/${syntax}/**/*.${syntax}`])
-		.pipe(sass({
-			'include css': true,
-			silenceDeprecations: ['legacy-js-api', 'mixed-decls', 'color-functions', 'global-builtin', 'import'],
-			loadPaths: ['./']
-		})).on('error', function handleError() { this.emit('end') })
-		.pipe(postCss([
-			autoprefixer({ grid: 'autoplace' }),
-			cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })
-		]))
-		.pipe(concat('main.min.css'))
-		.pipe(dest('app/css'))
-		.pipe(browserSync.stream())
+  return src([`app/${syntax}/**/*.${syntax}`])
+    .pipe(sass({
+      'include css': true,
+      silenceDeprecations: ['legacy-js-api', 'mixed-decls', 'color-functions', 'global-builtin', 'import'],
+      loadPaths: ['./']
+    })).on('error', function handleError() { this.emit('end') })
+    .pipe(postCss([
+      autoprefixer({ grid: 'autoplace' }),
+      cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })
+    ]))
+    .pipe(concat('main.min.css'))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
 function img1x() {
-	return src('app/img/_src/**/*.*', { encoding: false })
-	.pipe(imageResize({ width: '50%' }))
-	.pipe(dest('app/img/@1x/'))
+  return src('app/img/_src/**/*.*', { encoding: false })
+  .pipe(imageResize({ width: '50%' }))
+  .pipe(dest('app/img/@1x/'))
 }
 function img2x() {
-	return src('app/img/_src/**/*.*', { encoding: false })
-	.pipe(imageResize({ width: '100%' }))
-	.pipe(dest('app/img/@2x/'))
+  return src('app/img/_src/**/*.*', { encoding: false })
+  .pipe(imageResize({ width: '100%' }))
+  .pipe(dest('app/img/@2x/'))
 }
 async function cleanimg() {
-	await deleteAsync('app/img/@*', { force: true })
+  await deleteAsync('app/img/@*', { force: true })
 }
 
 function rsync() {
-	return src('app/') // Без звёздочек!
-		.pipe(rsyncModule({
-			root: 'app/',
-			hostname: 'username@yousite.com',
-			destination: 'yousite/public_html/',
-			clean: true, // Mirror copy with file deletion
-			// include: ['*.htaccess'], // Includes files to deploy
-			exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
-			recursive: true,
-			archive: true,
-			silent: false,
-			compress: true
-		}))
+  return src('app/') // Без звёздочек!
+    .pipe(rsyncModule({
+      root: 'app/',
+      hostname: 'username@yousite.com',
+      destination: 'yousite/public_html/',
+      clean: true, // Mirror copy with file deletion
+      // include: ['*.htaccess'], // Includes files to deploy
+      exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+      recursive: true,
+      archive: true,
+      silent: false,
+      compress: true
+    }))
 }
 
 function startwatch() {
-	watch([`app/${syntax}/**/*`], { usePolling: true }, styles)
-	watch(['app/js/common.js', 'app/libs/**/*.js'], { usePolling: true }, scripts)
-	watch([`app/**/*.{${fileswatch}}`], { usePolling: true }).on('change', browserSync.reload)
-	gmWatch && watch(['app/img/_src/**/*'], { usePolling: true }, img) // GraphicsMagick watching image sources if allowed
+  watch([`app/${syntax}/**/*`], { usePolling: true }, styles)
+  watch(['app/js/common.js', 'app/libs/**/*.js'], { usePolling: true }, scripts)
+  watch([`app/**/*.{${fileswatch}}`], { usePolling: true }).on('change', browserSync.reload)
+  gmWatch && watch(['app/img/_src/**/*'], { usePolling: true }, img) // GraphicsMagick watching image sources if allowed
 }
 
 export { scripts, styles, rsync, cleanimg }
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 <h1><strong>OptimizedHTML 4:</strong> <br>Startup HTML template based on Gulp & Bootstrap 5</h1>
 
 <p>
-	<img src="https://raw.githubusercontent.com/agragregra/OptimizedHTML-4/master/app/img/_src/preview.jpg" alt="Start HTML Template">
+  <img src="https://raw.githubusercontent.com/agragregra/OptimizedHTML-4/master/app/img/_src/preview.jpg" alt="Start HTML Template">
 </p>
 
 <p>Author: <a href="http://webdesign-master.ru" target="_blank">WebDesign Master</a></p>
@@ -17,34 +17,34 @@
 <h2>How to use OptimizedHTML</h2>
 
 <ol>
-	<li><a href="https://github.com/agragregra/OptimizedHTML-4/archive/master.zip">Download</a> <strong>OptimizedHTML 4</strong> from GitHub;</li>
-	<li>Install Node Modules: <strong>npm i</strong>;</li>
-	<li>
-		GraphicsMagick Required (opt. line 3): <br>
-		Install gm Linux/WSL: <code>sudo apt update; sudo apt install graphicsmagick</code> <br>
-		Install gm Git Bash: <a href="https://sourceforge.net/projects/graphicsmagick/files/" target="_blank">https://sourceforge.net/projects/graphicsmagick/files/</a>
-	</li>
-	<li>Run the template: <strong>gulp</strong>.</li>
+  <li><a href="https://github.com/agragregra/OptimizedHTML-4/archive/master.zip">Download</a> <strong>OptimizedHTML 4</strong> from GitHub;</li>
+  <li>Install Node Modules: <strong>npm i</strong>;</li>
+  <li>
+    GraphicsMagick Required (opt. line 3): <br>
+    Install gm Linux/WSL: <code>sudo apt update; sudo apt install graphicsmagick</code> <br>
+    Install gm Git Bash: <a href="https://sourceforge.net/projects/graphicsmagick/files/" target="_blank">https://sourceforge.net/projects/graphicsmagick/files/</a>
+  </li>
+  <li>Run the template: <strong>gulp</strong>.</li>
 </ol>
 
 <h2>Gulp tasks:</h2>
 
 <ul>
-	<li><strong>gulp</strong>: run default gulp task (sass, js, watch, browserSync) for web development;</li>
-	<li><strong>rsync</strong>: project deployment on the server from <strong>dist</strong> folder via <strong>RSYNC</strong>;</li>
+  <li><strong>gulp</strong>: run default gulp task (sass, js, watch, browserSync) for web development;</li>
+  <li><strong>rsync</strong>: project deployment on the server from <strong>dist</strong> folder via <strong>RSYNC</strong>;</li>
 </ul>
 
 <h2>Rules for working with the starting HTML template</h2>
 
 <ol>
-	<li>All HTML files should have similar initial content as in <strong>app/index.html</strong>;</li>
-	<li><strong>Template Basic Images Start</strong> comment in app/index.html - all your custom template basic images (og:image for social networking, favicons for a variety of devices);</li>
-	<li><strong>Custom Browsers Color Start</strong> comment in app/index.html: set the color of the browser head on a variety of devices;</li>
-	<li><strong>Custom HTML</strong> comment in app/index.html - all your custom HTML;</li>
-	<li>For installing new jQuery library, just run the command "<strong>bower i plugin-name</strong>" in the terminal. Libraries are automatically placed in the folder <strong>app/libs</strong>. Bower must be installed in the system (npm i -g bower). Then place all jQuery libraries paths in the <strong>'libs'</strong> task (gulpfile.js);</li>
-	<li>All custom JS located in <strong>app/js/common.js</strong>;</li>
-	<li>All Sass vars placed in <strong>app/sass/_vars.sass | app/scss/_vars.scss</strong>;</li>
-	<li>All Bootstrap media queries placed in <strong>app/sass/_media.sass | app/scss/_media.scss</strong>;</li>
-	<li>All libraries CSS styles placed in <strong>app/sass/_libs.sass | app/scss/_libs.scss</strong>;</li>
-	<li>Rename <strong>ht.access</strong> to <strong>.htaccess</strong> before place it in your web server. This file contain rules for files caching on web server.</li>
+  <li>All HTML files should have similar initial content as in <strong>app/index.html</strong>;</li>
+  <li><strong>Template Basic Images Start</strong> comment in app/index.html - all your custom template basic images (og:image for social networking, favicons for a variety of devices);</li>
+  <li><strong>Custom Browsers Color Start</strong> comment in app/index.html: set the color of the browser head on a variety of devices;</li>
+  <li><strong>Custom HTML</strong> comment in app/index.html - all your custom HTML;</li>
+  <li>For installing new jQuery library, just run the command "<strong>bower i plugin-name</strong>" in the terminal. Libraries are automatically placed in the folder <strong>app/libs</strong>. Bower must be installed in the system (npm i -g bower). Then place all jQuery libraries paths in the <strong>'libs'</strong> task (gulpfile.js);</li>
+  <li>All custom JS located in <strong>app/js/common.js</strong>;</li>
+  <li>All Sass vars placed in <strong>app/sass/_vars.sass | app/scss/_vars.scss</strong>;</li>
+  <li>All Bootstrap media queries placed in <strong>app/sass/_media.sass | app/scss/_media.scss</strong>;</li>
+  <li>All libraries CSS styles placed in <strong>app/sass/_libs.sass | app/scss/_libs.scss</strong>;</li>
+  <li>Rename <strong>ht.access</strong> to <strong>.htaccess</strong> before place it in your web server. This file contain rules for files caching on web server.</li>
 </ol>
